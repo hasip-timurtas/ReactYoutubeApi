195adba74905e3ab6d53ee9914f05a7197432c9d
hasip <hasip.timurtas@gmail.com>
2016-05-29 02:08:16 +0300

    arranged npm packages

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+export default class App extends React.Component{
+    render(){
+        return (<div>Hasip</div>)
+    }
+}
+
+ReactDOM.render(<App />,document.querySelector('.container'));
